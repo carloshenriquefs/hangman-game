dbe52c19746d1762b7984dfe01ef1779088f712e
Carlos Henrique <ichfs9108@gmail.com>
2024-01-17 23:35:26 -0300

    Project created

---
+++ hangman-game/hangman-game/src/main/java/com/hangman/game/HangmanGameApplication.java
@@ -0,0 +1,13 @@
+package com.hangman.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangmanGameApplication.class, args);
+	}
+
+}
